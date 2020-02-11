b34f3b0fda8ee275c3108bcab9369ea8ed7d00c1
msegnit <melsegnit@gmail.com>
2020-02-11 15:07:06 -0600

    "updated pusheen"

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/*----- constants -----*/
+var pusheens = ['images/birthdaypusheen.png', 'images/pusheennoods.png', 'images/unicornpusheen.png']
+
+
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+
+let img1 = document.getElementById('1');
+let img2 = document.getElementById('2');
+let img3 = document.getElementById('3');
+let message = document.getElementById('message');
+
+/*----- event listeners -----*/
+
+let spin = document.getElementById('button').addEventListener('click', handleSpin)
+
+
+
+/*----- functions -----*/
+
+function handleSpin() {
+   let idx = Math.floor(Math.random() * Math.floor(pusheens.length -1)); 
+   img1.src= pusheens[idx];
+   let idx2 = Math.floor(Math.random() * Math.floor(pusheens.length -1)); 
+   img2.src= pusheens[idx2];
+   let idx3 = Math.floor(Math.random() * Math.floor(pusheens.length -1)); 
+   img3.src= pusheens[idx3];
+}
+
+function catWinner() {
+    if (img1.src === img2.src && img3.src === img1.src){
+        message.textContent = `Winner! Winner!`
+    }
+    
+}
